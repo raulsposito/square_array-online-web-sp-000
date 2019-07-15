d7f6a43bd5cf840c1259f186a6dc21bcff2a1e74
Raul Sposito <raulsposito@gmail.com>
2019-07-15 15:17:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,6 @@
-def square_array(array)
-  array.each |array**array|
+def square_array(numbers)
+  numbers = [1, 2, 3]
+  numbers.each |number**number|
+  new_numbers = [1, 4, 9]
+  square_array(new_numbers)
 end
