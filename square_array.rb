0cd77c3b23151fd15d4ccf2a7db9a52b610c3dd8
Raul Sposito <raulsposito@gmail.com>
2019-07-15 15:23:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
 def square_array(numbers)
   numbers = [1, 2, 3]
-  numbers.each {|numbers| ** print new_numbers}
+  numbers.each {|numbers| ** numbers print new_numbers}
   square_array(new_numbers)
 end
 
